4cc4ace74a0bf1ae84fc114e2a9e39fc97f83234
alexis-valois <alexis@ugotspotted.com>
2014-02-01 14:18:44 -0500

    creation de compte utilisateur

--- usermanagement/urls.py
+++ usermanagement/urls.py
@@ -1,6 +1,8 @@
 __author__ = 'Alexis'
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from views import create_account
 urlpatterns = patterns('',
-    url(r'^succes/$', TemplateView.as_view(template_name="user/succes.html"))
+    url(r'^succes/$', TemplateView.as_view(template_name="user/succes.html")),
+    url(r'^create_account/$', create_account),
 )
