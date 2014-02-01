1be82d1929b349c0cf8814b1f962ecc35c7e2a57
alexis-valois <alexis@ugotspotted.com>
2014-02-01 13:17:25 -0500

    Authentication

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.auth.views import login, logout
 from django.contrib import admin
+from personal_calender.views import profile
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,4 +11,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', login),
+    url(r'^accounts/logout/$', logout, {'next_page' : 'accounts/login'}),
+    url(r'^accounts/profile/$', profile),
+
 )
