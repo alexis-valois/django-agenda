2c8f286fd4983e83cf2fc0670e78559982fcfebc
unknown <alexis.valois@hotmail.com>
2014-02-03 22:58:54 -0500

    vue details terminée

--- personal_calender/urls.py
+++ personal_calender/urls.py
@@ -1,8 +1,9 @@
 __author__ = 'Alexis'
-from django.conf.urls import  patterns, include, url
-from views import create, details
+from django.conf.urls import  patterns, url
+from views import create, details, delete
 
 urlpatterns = patterns('',
     url(r'^create/$', create),
     url(r'^(\d+)/details/$',details),
+    url(r'^(\d+)/participant/(\d+)/delete/$', delete),
 )
--- personal_calender/views.py
+++ personal_calender/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponseRedirect
 from forms import EventForm, Evenement_ParticipantForm
-from models import Evenement
+from models import Evenement, Evenement_Participant
+from django.contrib.auth.models import User
+from django.forms import HiddenInput
 
 def create(request):
     if request.method == "POST":
@@ -18,9 +20,23 @@ def details(request, id):
         form = Evenement_ParticipantForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect('/agenda/%s/details/' % event)
+            return HttpResponseRedirect('/agenda/%s/details/' % id)
     else:
-        form = Evenement_ParticipantForm()
+        form = Evenement_ParticipantForm(initial = {'evenement': event})
+        participants = [user.pk for user in event.participants.all()]
+        form.fields['participant'].queryset = User.objects.exclude(pk__in = participants)
+        form.fields['evenement'].widget = HiddenInput()
     return render(request, 'event/details.html',
                   {'event':event,
                    'form': form})
+
+def delete(request, id, participant):
+    if request.method == "POST":
+        evenement = Evenement.objects.get(pk = id)
+        participant = User.objects.get(pk = participant)
+        a_supprimer = Evenement_Participant.objects.get(
+            evenement = evenement,
+            participant = participant
+        )
+        a_supprimer.delete()
+    return HttpResponseRedirect('/agenda/%s/details/' %id)
