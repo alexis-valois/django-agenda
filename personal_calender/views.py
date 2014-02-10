022946158462f0b457ebc3c9490a9fe24812d1cb
alexis-valois <alexis.valois@hotmail.com>
2014-02-09 21:48:43 -0500

    Interface Ajax pour ajout participant événement

--- personal_calender/views.py
+++ personal_calender/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, render_to_response
 from forms import EventForm, Evenement_ParticipantForm
 from models import Evenement, Evenement_Participant
 from django.contrib.auth.models import User
 from django.forms import HiddenInput
+from django.template.loader import render_to_string
+from django.template import RequestContext
+from django.http import HttpResponse
+import json
 
 def create(request):
     if request.method == "POST":
@@ -15,20 +19,41 @@ def create(request):
     return render(request,'personal_calender/event/create.html', {'form':form})
 
 def details(request, id):
+
     event = Evenement.objects.get(pk = id)
     if request.method == "POST":
         form = Evenement_ParticipantForm(request.POST)
         if form.is_valid():
             form.save()
+            if request.is_ajax():
+                delete_form = render_to_string('personal_calender/blocks/delete_form.html',
+                                               {'delete_url': form.instance.delete_url(),},
+                                               RequestContext(request)
+                )
+                data = {'participant' : form.instance.participant.username,
+                        'get_status_display': form.instance.get_status_display(),
+                        'delete_form' : delete_form
+                }
+                return HttpResponse(
+                    json.dumps(data),
+                    mimetype="application/json"
+                )
             return HttpResponseRedirect('/agenda/%s/details/' % id)
     else:
         form = Evenement_ParticipantForm(initial = {'evenement': event})
         participants = [user.pk for user in event.participants.all()]
         form.fields['participant'].queryset = User.objects.exclude(pk__in = participants)
         form.fields['evenement'].widget = HiddenInput()
+    if request.is_ajax():
+        return render_to_response(
+            'personal_calender/blocks/participant_form.html',
+            {'event' : event,
+             'form': form}
+        )
+
     return render(request, 'personal_calender/event/details.html',
-                  {'event':event,
-                   'form': form})
+        {'event':event,
+        'form': form})
 
 def delete(request, id, participant):
     if request.method == "POST":
@@ -39,6 +64,10 @@ def delete(request, id, participant):
             participant = participant
         )
         a_supprimer.delete()
+
+    if request.is_ajax():
+        return HttpResponse("OK")
+
     return HttpResponseRedirect('/agenda/%s/details/' %id)
 
 def liste(request):
